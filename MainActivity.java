34c189ba669cfa0fb956b2a18484ea606e129e6a
rebot715 <rebot715@gmail.com>
2016-03-30 21:20:23 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -36,7 +36,6 @@
     import android.media.MediaRecorder;
     import android.media.MediaPlayer;
 
-
     public class MainActivity extends Activity {
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
@@ -65,34 +64,29 @@
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        // set a custom shadow that overlays the main content when the drawer opens
         mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        // set up the drawer's list view with items and click listener
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.drawer_list_item, mRoom));
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        // enable ActionBar app icon to behave as action to toggle nav drawer
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
 
-        // ActionBarDrawerToggle ties together the the proper interactions
-        // between the sliding drawer and the action bar app icon
         mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description for accessibility */
-                R.string.drawer_close  /* "close drawer" description for accessibility */
+                this,                 
+                mDrawerLayout,         
+                R.drawable.ic_drawer,  
+                R.string.drawer_open,  
+                R.string.drawer_close  
         ) {
             public void onDrawerClosed(View view) {
                 getActionBar().setTitle(mRoom[position]);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                invalidateOptionsMenu(); 
             }
 
             public void onDrawerOpened(View drawerView) {
                 getActionBar().setTitle("Rooms");
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                invalidateOptionsMenu(); 
             }
         };
         mDrawerLayout.setDrawerListener(mDrawerToggle);
@@ -109,10 +103,9 @@
         return super.onCreateOptionsMenu(menu);
     }
 
-    /* Called whenever we call invalidateOptionsMenu() */
+    
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
         boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
         menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
         return super.onPrepareOptionsMenu(menu);
@@ -120,18 +113,13 @@
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-        // Handle action buttons
         switch(item.getItemId()) {
             case R.id.action_websearch:
-                // create intent to perform web search for this room
                 Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
                 intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
-                // catch event that there's no activity to handle intent
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivity(intent);
                 } else {
@@ -163,7 +151,6 @@
         startActivity(d);
     }
 
-    /* The click listner for ListView in the navigation drawer */
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -206,33 +193,23 @@
         mTitle = title;
     }
 
-    /**
-     * When using the ActionBarDrawerToggle, you must call it during
-     * onPostCreate() and onConfigurationChanged()...
-     */
-
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
         mDrawerToggle.syncState();
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggles
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
-    /**
-     * Fragment that appears in the "content_frame", shows a room
-     */
     public static class RoomFragment extends Fragment {
         public static final String ARG_ROOM_NUMBER = "room_number";
 
         public RoomFragment() {
-            // Empty constructor required for fragment subclasses
+            //empty constructor for subclasses
         }
 
         @Override
@@ -244,10 +221,7 @@
             String room = getResources().getStringArray(R.array.rooms_array)[i];
             int imageId = getResources().getIdentifier(room.toLowerCase(Locale.getDefault()),
                     "drawable", getActivity().getPackageName());
-
             //changes the image on room selection
-            //((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
-
             getActivity().setTitle(room);
             return rootView;
         }
@@ -355,8 +329,3 @@
             Log.d("VIVZ", "Player Released");}
         }
     }
-    
-    /*PackageManager packageManager = getPackageManager();
-    List activities = packageManager.queryIntentActivities(intent,
-            PackageManager.MATCH_DEFAULT_ONLY);
-    boolean isIntentSafe = activities.size() > 0;*/
